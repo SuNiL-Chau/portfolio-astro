45e1f4b500316c7b02b1d5acfa2d9154ade84096
Sunil Chaudhary <1234huts@gmail.com>
2023-05-15 00:21:26 +0530

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://sc-portfolio.netlify.app",
+  integrations: [sitemap()],
+  output: "server",
+  adapter: netlify()
+});
